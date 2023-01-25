26b0bdae830c3043698291a9595534000e4219c1
RowinVA <rowin.amsterdam@gmail.com>
2023-01-25 14:50:33 +0100

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./apps/app-portal/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
