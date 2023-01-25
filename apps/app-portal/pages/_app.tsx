e618555f756c8b67260a73f25ae2fc2c33109cce
RowinVA <rowin.amsterdam@gmail.com>
2023-01-25 15:01:35 +0100

    Test branch protection

--- apps/app-portal/pages/_app.tsx
+++ apps/app-portal/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to app-portal!</title>
+        <title>Welcome to app-portal! </title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
