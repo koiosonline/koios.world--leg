6cfe1452774bc1be9835faccd66559246fca6056
RowinVA <rowin.amsterdam@gmail.com>
2023-02-08 14:41:18 +0100

    Added colors

---
+++ apps/app-portal/src/stories/Colors.stories.tsx
@@ -0,0 +1,13 @@
+const Meta = {
+    title: 'Colors',
+  };
+
+  export const Default = () => {
+    return (
+      <>
+        <h2 className="h1">Colors</h2>
+      </>
+    );
+  };
+
+  export default Meta;
