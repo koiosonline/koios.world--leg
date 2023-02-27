6ca0d754b2e8c1a0bd91c251285e28c66a8716a6
RowinVA <rowin.amsterdam@gmail.com>
2023-02-27 20:24:28 +0100

    fix hydration issues rainbowkit

--- apps/app-portal/src/providers/RainbowKitProvider.tsx
+++ apps/app-portal/src/providers/RainbowKitProvider.tsx
@@ -1,5 +1,7 @@
 import { AvatarComponent, getDefaultWallets, lightTheme, RainbowKitProvider as RainbowProvider } from '@rainbow-me/rainbowkit';
 import Image from 'next/image';
+import React from 'react';
+import { useEffect, useState } from 'react';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { mainnet, polygon } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
@@ -9,7 +11,8 @@ type RainbowKitProviderType = {
   children: React.ReactNode;
 };
 
-const { chains, provider } = configureChains([mainnet, polygon], [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]);
+const alchemyProviderApiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_ETH_MAIN;
+const { chains, provider } = configureChains([mainnet, polygon], [alchemyProvider({ apiKey: alchemyProviderApiKey }), publicProvider()]);
 
 const { connectors } = getDefaultWallets({
   appName: 'Koios app-portal',
@@ -27,24 +30,34 @@ const CustomAvatar: AvatarComponent = ({ ensImage, size }) => {
   return <Image src={pfp} alt="Avatar" width={size} height={size} style={{ borderRadius: 4 }} />;
 };
 
-
-
 export const RainbowKitProvider = ({ children }: RainbowKitProviderType) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    // Prevents SSR hydration issues with RainbowKit
+    setIsMounted(true);
+  }, []);
+
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowProvider
-        coolMode
-        chains={chains}
-        avatar={CustomAvatar}
-        theme={lightTheme({
-          accentColor: '#512C9E',
-          accentColorForeground: 'white',
-          borderRadius: 'small',
-          fontStack: 'system'
-        })}
-      >
-        {children}
-      </RainbowProvider>
-    </WagmiConfig>
+    <>
+      {isMounted ? (
+        <WagmiConfig client={wagmiClient}>
+          <RainbowProvider
+            chains={chains}
+            avatar={CustomAvatar}
+            theme={lightTheme({
+              accentColor: '#512C9E',
+              accentColorForeground: 'white',
+              borderRadius: 'small',
+              fontStack: 'system'
+            })}
+          >
+            {children}
+          </RainbowProvider>
+        </WagmiConfig>
+      ) : (
+        <React.Fragment />
+      )}
+    </>
   );
 };
